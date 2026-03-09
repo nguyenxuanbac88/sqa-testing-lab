93b4eea66d010b0ae74a5278067e135c923815a9
nguyenxuanbac88 <xuanbacatym@gmail.com>
2026-03-09 10:27:18 +0700

    first commit

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace sqa_automation_testing
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
